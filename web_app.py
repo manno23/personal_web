55ddf1ca7c80cb4052aff6bb201ee06f1f6316fb
Jason Manning <jasonmanning23@gmail.com>
2017-01-31 04:21:12 +0800

    Added post templates and a whole bunch of pictures.

--- web_app.py
+++ web_app.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+blog_entries = ['ssd', 'hydra', 'firewall', 'firmware']
+
+
+
 @app.route("/") 
 def root():
     return render_template('root.html')
 
-@app.route('/blog')
-@app.route('/blog/<name>')
+article = {
+    'ssd': "This is the article for the ssd",
+    'hydra': "This is the article for hydra",
+    'firewall': "This is the article for the firewall",
+    'firmware': "This is the article for firmware"
+}
+
+@app.route('/<name>')
+@app.errorhandler(404)
 def blog(name=None):
-    return render_template('blog.html', name=name)
+    if name == 'ssd':
+        return render_template('ssd.html', name=name)
+    if name in blog_entries:
+        return render_template('post.html', name=name, article=article[name])
+    else:
+        return render_template('missing.html'), 404
+
 
 if __name__ == "__main__":
     app.run()
+
